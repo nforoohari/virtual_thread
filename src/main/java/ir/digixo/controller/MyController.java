55b81ccb9219b84d525cfa6c2b318419375010a5
Nima Foroohari <nforoohari@gmail.com>
2025-06-09 19:22:13 +0330

    Version 0

---
+++ src/main/java/ir/digixo/controller/MyController.java
@@ -0,0 +1,22 @@
+package ir.digixo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vt")
+public class MyController {
+    @GetMapping
+    public String vt()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Thread.currentThread().getName();
+    }
+}
