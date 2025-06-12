66a0d1160591d312c345584617895843452e16d8
Nima Foroohari <nforoohari@gmail.com>
2025-06-12 15:06:42 +0330

    Version 1

--- src/main/java/ir/digixo/controller/MyController.java
+++ src/main/java/ir/digixo/controller/MyController.java
@@ -1,6 +1,5 @@
 package ir.digixo.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/vt")
 public class MyController {
+
     @GetMapping
-    public String vt()
-    {
+    public String vt() {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
         return Thread.currentThread().getName();
     }
 }
